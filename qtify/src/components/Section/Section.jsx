27153b98f11549dbf47b3411f06f1920db3e2057
anandabhijeet98 <anandabhijeet98@gmail.com>
2024-02-01 16:26:18 +0530

    Album Check

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -2,9 +2,10 @@ import React, {useState} from "react";
 import styles from "./Section.module.css";
 import { CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
+import Carousel from "../Carousel/Carousel";
 
 export default function Section({title, data, type}){
-    const [carouselToggle, setCarouselToggle]=useState(false);
+    const [carouselToggle, setCarouselToggle]=useState(true);
 
     const handleToggle=()=>{
         setCarouselToggle((prevState)=> !prevState);
@@ -29,7 +30,10 @@ export default function Section({title, data, type}){
                             ))}
                             </div>
                         ) : (
-                            <div>Carousel</div>
+                            <Carousel 
+                            data={data} 
+                            renderComponent={(d)=> <Card data={d} type={type}/>}
+                            />
                         )}                     
                         
                     </div>
